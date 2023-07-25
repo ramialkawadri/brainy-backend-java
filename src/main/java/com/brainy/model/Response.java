9f97f8a50c43e6923b39ad4c1490ce2aa9974c0d
Rami Alkawadri <ramialkawadri@hotmail.com>
2023-07-25 23:52:20 +0200

    Remove unused annotation in Response class

--- src/main/java/com/brainy/model/Response.java
+++ src/main/java/com/brainy/model/Response.java
@@ -1,13 +1,19 @@
 package com.brainy.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class Response<T> {
-    private T data;
     private ResponseStatus status;
+    private T data;
 
     public Response(T body, ResponseStatus status) {
         this.data = body;
+        this.status = status;
+    }
+
+    public ResponseStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ResponseStatus status) {
         this.status = status;
     }
 
@@ -17,14 +23,5 @@ public class Response<T> {
 
     public void setData(T body) {
         this.data = body;
-    }
-
-    @JsonProperty("status")
-    public ResponseStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(ResponseStatus status) {
-        this.status = status;
     }
 }
