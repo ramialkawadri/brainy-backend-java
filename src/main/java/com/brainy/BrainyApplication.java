964b6bf34504704e60cf7827b2cf478d87c92afd
Rami Alkawadri <ramialkawadri@hotmail.com>
2023-07-25 22:48:16 +0200

    Init

---
+++ src/main/java/com/brainy/BrainyApplication.java
@@ -0,0 +1,13 @@
+package com.brainy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brainy/BrainyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brainy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
